001fb3f3303414075a6adb9f04859ad952a32cb1
boogie0man <shanidshalu@gmail.com>
2025-11-16 23:57:48 +0530

    Completed the course

---
+++ eslint.config.mjs
@@ -0,0 +1,37 @@
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ]),
+  // Override for non-TS/TSX files
+  {
+    files: ['**/*.{js,jsx,mjs,cjs}'],
+    rules: {
+      // Add rules for non-TypeScript files here
+    },
+  },
+  // Enable accessibility rules for TSX/JSX files
+  {
+    files: ['**/*.{ts,tsx,js,jsx}'],
+    rules: {
+      'jsx-a11y/alt-text': 'error',
+      'jsx-a11y/anchor-has-content': 'error',
+      'jsx-a11y/aria-role': 'error',
+      'jsx-a11y/click-events-have-key-events': 'error',
+      'jsx-a11y/label-has-associated-control': 'error',
+      'jsx-a11y/no-static-element-interactions': 'error',
+    },
+  },
+]);
+
+export default eslintConfig;
